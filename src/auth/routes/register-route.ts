620ffd63b54dab75ffaa25a26f8a5276ab31aceb
jlerocher <jlerocher@gmail.com>
2025-06-08 14:58:20 +0100

    feat: update configuration files and improve code formatting

--- src/auth/routes/register-route.ts
+++ src/auth/routes/register-route.ts
@@ -3,8 +3,8 @@ import { Hono } from "hono";
 const authRouter = new Hono();
 
 authRouter.post("/register", async (c) => {
-  const { username, password } = await c.req.json();
-  console.log(password);
+    const { username, password } = await c.req.json();
+    console.log(password);
 
-  return c.json({ message: `User ${username} registered successfully!` });
+    return c.json({ message: `User ${username} registered successfully!` });
 });
